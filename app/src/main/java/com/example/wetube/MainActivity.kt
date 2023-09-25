5709a5255c4527f7f7726b7d9c756e45eac1b32a
minji-0420 <pongiii17@gmail.com>
2023-09-25 14:12:31 +0900

    ➕ ADD : MainActivity Navigation 연결

--- app/src/main/java/com/example/wetube/MainActivity.kt
+++ app/src/main/java/com/example/wetube/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.wetube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.wetube.databinding.ActivityMainBinding
 
+
+// https://teamsparta.notion.site/e9cce6173ed04fbc871da4818668c4e6
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(binding.root)
+        setUpBottomNavigationView()
+    }
+    private fun setUpBottomNavigationView() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
