89b0f2015c0f485dcd42a5555244ba6cb108ec50
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-10-06 08:24:01 +0900

    [FEAT] : ⚙️ 뒤로가기 두번 클릭 시 이벤트 작성

--- app/src/main/java/com/example/wetube/MainActivity.kt
+++ app/src/main/java/com/example/wetube/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.wetube
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.wetube.databinding.ActivityMainBinding
@@ -12,7 +16,7 @@ import com.example.wetube.ui.mypage.MypageFragment
 class MainActivity : AppCompatActivity() {
     private lateinit var homeFragment: HomeFragment
     private lateinit var mypageFragment: MypageFragment
-
+private var mBackWait:Long = 0
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setUpBottomNavigationView()
+
+        this.onBackPressedDispatcher.addCallback(this, callback)
+
     }
 
     private fun setUpBottomNavigationView() = binding.apply {
@@ -62,5 +69,16 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         overridePendingTransition(R.anim.enter_slide_down, R.anim.exit_slide_down)
+    }
+
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if(System.currentTimeMillis() - mBackWait >= 2000) {
+                mBackWait = System.currentTimeMillis()
+                Toast.makeText(this@MainActivity, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                finish()
+            }
+        }
     }
 }
